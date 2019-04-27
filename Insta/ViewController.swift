f02e4b7fa7f2f34571cd8fbf6a50f6a2a23574f2
Ryan Luu <ryanluu12345@gmail.com>
2019-04-27 12:51:03 -0700

    Initial Commit

---
+++ Insta/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Insta
+//
+//  Created by Ryan Luu on 4/27/19.
+//  Copyright © 2019 rnluu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
